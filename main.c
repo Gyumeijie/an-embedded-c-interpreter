e0df91026d58e0c75dde52365337d41890201267
Gyumeijie <18170879819@163.com>
2018-04-07 21:09:07 +0800

    refactor: change function name from depedenncy_inject to compile_src_code

--- main.c
+++ main.c
@@ -7,51 +7,22 @@ int main()
 {
    parser_init();
    executor_init();
-   char* src;
-   char* dependency;
 
-   /*
-   double test_extern;
-   src = "use{int j; float i;} action{j=33; i = 1.3; test_extern = 11.2 + i + 1.5;}";
-   dependency = "test_extern";
-   int* code1 = dependency_inject(dependency, &test_extern, src);
-
-   test_extern = 34;
-   run_code(code1);
-   printf("test_extern is %lf\n", test_extern);
-   */
-
-   
-   double test_extern = 10;
-   int  result;
-
+   int data = 13; 
+   int result;
    struct dependency_items* dep_itemsp;
    dep_itemsp = init_dependency_items(2);
-   add_dependency_item(dep_itemsp, "test_extern", &test_extern, DOUBLE);
+   add_dependency_item(dep_itemsp, "data", &data, INT);
    add_dependency_item(dep_itemsp, "result", &result, INT);
-   src = "use{double f;} action{result = (float*)test_extern; }";
+
+   char* src = "use{} action{result = data * 6;}";
    
-   int* code1 = dependency_inject(dep_itemsp, src);
-
+   int* code1 = compile_src_code(dep_itemsp, src);
    run_code(code1);
-   printf("test_extern is  %lf, result is %d\n",  test_extern, result);
 
-   /*
-   src = "use{int iii = 2; int k = 3;} action{printf(\"test_extern is %d\n\", test_extern + iii*k);}";
-   dependency = "test_extern";
-   int* code2 = dependency_inject(dependency, &test_extern, src);
-
-   test_extern = 35;
-   run_code(code2);
-   */ 
-
-
-   /*
-   src = "use{int i = 3; int j = 4;} action{if(i > j){test_extern = 100 - i + j;} else if (i == 2){test_extern = 100;} else{test_extern = 0; while (i--) { test_extern++;}} }";
-   dependency = "test_extern";
-   int* code2 = dependency_inject(dependency, &test_extern, src);
-   run_code(code2);
-   printf("test_extern is %d\n", test_extern);
-   */
+   printf("result is %d\n",  result);
+   printf("result %p, data %p:", &result, &data);
+ 
+   printf("IMM:%0x PUSH:%0x LI:%0x MUL:%0x  SI:%0x  ADD:%0x EXIT:%0x\n", IMM, PUSH, LI, MUL, SI, ADD, EXIT);
 
 }
